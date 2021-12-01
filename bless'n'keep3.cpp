e443cd6d0e66671f30185286cb078146bc6d7521
ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>
2021-12-01 09:50:23 +0500

    base class
    Signed-off-by: ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>

---
+++ bless'n'keep3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+const int n = 10;
+
+class object {
+private:
+	char name;
+public:
+	object(char s) {
+		name = s;
+		cout << "the object " << name << " has been created\n";
+	}
+	void setname(char s) {
+		name = s;
+		cout << "the name " << name << " has been setted\n";
+	}
+	char getname() {
+		return name;
+	}
+	~object() {
+		cout << "the object " << name << " has been deleted\n";
+	}
+};
